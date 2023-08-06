733487aadaea9b3fd903e489e3cfd5f2be90edbc
Thakshila navodya watadenithanna <65487716+Thakshila-N@users.noreply.github.com>
2023-08-06 13:25:16 +0530

    Login and Sigup api

---
+++ AngularAuthAPI/Context/AddDBContext.cs
@@ -0,0 +1,20 @@
+﻿using AngularAuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthAPI.Context
+{
+    public class AddDBContext: DbContext
+    {
+        public AddDBContext(DbContextOptions<AddDBContext> options):base(options) { }
+
+
+        public DbSet<User> Users { get; set; }
+    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");   
+        }
+    }
+
+}
---
+++ AngularAuthAPI/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AddDBContext _authContext;
+
+        public UserController(AddDBContext addDBContext)
+        {
+            _authContext = addDBContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if(userObj == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username && x.Password == userObj.Password);
+
+            if(user == null)
+            {
+                return NotFound(new {Message = "User Not Found"});
+
+            }
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj == null)
+            {
+                return BadRequest();
+            }
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
